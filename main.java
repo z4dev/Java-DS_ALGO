6c0e25b10e8ed7e2d0df708df8c8ec33a21fd0a4
Izzedeen Alfarra <z4dev@Izzedeens-MacBook-Pro.local>
2024-12-02 09:26:34 +0200

    feat: default constructor extends

--- main.java
+++ main.java
@@ -3,9 +3,50 @@ package Java ;
 import static  Java.Printer.print; 
 
 
+class Apply {
+    private int age; 
+    private int name;
+    Apply(){}
+    Apply(int age, int name){
+        this.age = age;
+        this.name = name;
+    }
+    String display(){
+        return "Age: " + age + " Name: " + name;
+    }
+    void setter(int age, int name){
+        age = age;
+        name = name;
+    }
+}
+
+// super => parent class 
+class Pre extends Apply{
+
+    boolean isGeeks = true;
+
+    Pre(){
+        super();
+    }
+
+    Pre(int age, int name){
+        super(age, name);
+    }
+
+    @Override
+     String display(){
+         String output = super.display();
+         setter(40,30);
+         return output + " isGeeks: " + this.isGeeks;
+         
+    }
+}
 
 class HelloWorld {
     public static void main(String[] args) {
-        print("hello world");
+        Pre obj = new Pre(10, 2);
+        print(obj.display());
     }
 }
+
+
