10dc25d8461d296cd14a350d2bced46b1eca29e3
Izzedeen Alfarra <ezazozqq123321@gmail.com>
2024-12-09 11:22:59 +0200

    feat : ArrayList and insertion

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,6 +1,5 @@
 package Java;
 import static Java.Printer.print;
-
 import java.util.Arrays;
 
 public class MyArrayList<T>{
@@ -16,17 +15,28 @@ public class MyArrayList<T>{
 
     //add element to the end of the list 
     //at the end of the exists data , not the end of the capacity which it is 10
-    public void add(Object element){
-        if(elements.length == size){resize();}
-        elements[size] = (T) element ;
-        size+=1;
+    public void add(T element){
+        checkCapacityGuard(this.size, this.elements);
+        elements[size] = element ;
+        this.size+=1;
     }
-    public T[] get(){
-        return elements;
+
+    public void add(T element , int index){
+
+          checkCapacityGuard(this.size, this.elements);
+          checkIndexGuard(index);
+            for(int i = index-1 ; i <= size ; i++){
+                elements[i+1] = elements[i];
+          }
+
+          elements[index-1] = element;
+          this.size+=1;
+
     }
+
     private void resize(){
-        int newCapicty = 2 * elements.length;
-        elements = Arrays.copyOf(elements, newCapicty);
+        int newCapacity = 2 * elements.length;
+        elements = Arrays.copyOf(elements, newCapacity);
 
     }
     public void print(){
@@ -38,33 +48,40 @@ public class MyArrayList<T>{
             }
         }
         Java.Printer.println("]");
+    
+    };
+
+    public void checkIndexGuard(int index) {
+
+        if(index>0 || index > size){
+
+            throw new IllegalArgumentException("Invalid Index: " + index);
+
+        }
+    }
+
+    public void checkCapacityGuard(int size , T[] elements){
+          if(elements.length > size){
+            resize();
+          }
     }
     
 
 
     public static void main(String args[]){
         MyArrayList<String> myArr = new MyArrayList<>();
-        myArr.add("Apple");
-        myArr.add("Banana");
-        myArr.add(1);
-        myArr.add(true);
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add("Banana");
-        myArr.add(10d);
-        myArr.add(3.2f);
-
+        myArr.add("myArr");
+        myArr.add("myArr2");
+        myArr.add("myArr3");
+        myArr.add("myArr4");
+    
 
         myArr.print();
+        System.err.println(myArr.size);
+
+
+
+        
         
 
 
