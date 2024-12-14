4c8f0ad48758e67f5dde0f05383d206bfcbb2814
Izzedeen Alfarra <ezazozqq123321@gmail.com>
2024-12-14 10:26:39 +0200

    feat : added some methods into ArrayList

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,5 +1,8 @@
 package Java;
 import static Java.Printer.print;
+import static Java.Printer.println;
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MyArrayList<T>{
@@ -13,6 +16,32 @@ public class MyArrayList<T>{
         elements =  (T[]) new Object[DEFAULT_CAPACITY];
     }
 
+
+    public T get(int index){
+        checkIndexGuard(index);
+        return elements[index];
+    }
+    public void set(int index){
+        checkIndexGuard(index);
+
+
+    }
+
+    public void clear(){
+        this.size = 0 ; 
+        elements =  (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public T[] removeAt(int index){
+        for(int j = index ;j <= size-1; j++){
+            elements[j] = elements[j+1];
+        }
+        this.size-=1;
+        elements[size] = null; // Optional, helps with garbage collection for object arrays
+
+        return elements;
+
+    }
     //add element to the end of the list 
     //at the end of the exists data , not the end of the capacity which it is 10
     public void add(T element){
@@ -21,11 +50,15 @@ public class MyArrayList<T>{
         this.size+=1;
     }
 
+ 
+
+
+
     public void add(T element , int index){
 
           checkCapacityGuard(this.size, this.elements);
           checkIndexGuard(index);
-            for(int i = index-1 ; i <= size ; i++){
+            for(int i = index; i <= size ; i++){
                 elements[i+1] = elements[i];
           }
 
@@ -69,22 +102,12 @@ public class MyArrayList<T>{
 
 
     public static void main(String args[]){
-        MyArrayList<String> myArr = new MyArrayList<>();
+        MyArrayList<Object> myArr = new MyArrayList<>();
         myArr.add("myArr");
         myArr.add("myArr2");
         myArr.add("myArr3");
-        myArr.add("myArr4");
-    
-
+        myArr.add("myArr4");  
         myArr.print();
-        System.err.println(myArr.size);
-
-
-
-        
-        
-
-
     }
 }
 
